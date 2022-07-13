6531eb50bf0c76adc3da97bef3375d60df5b353d
lvjiaqing <732210512@qq.com>
2022-07-13 23:50:42 +0800

    fix:数据连接错误处理 socp: src/lib/db

--- src/lib/db/mod.rs
+++ src/lib/db/mod.rs
@@ -17,15 +17,20 @@ pub struct DBPool {
 
 /// 初始化数据库池
 pub async fn new_dbpool(conf: &ServerConfig) -> Result<DBPool, Box<dyn Error>> {
-    let pg = PgPoolOptions::new()
+    let pg_res= PgPoolOptions::new()
         .max_connections(conf.pg.max_size)
         .min_connections(conf.pg.min_size)
         .idle_timeout(Duration::new(conf.pg.idle_timeout, 0))
-        .connect(&conf.pg.connect_str).await?;
+        .connect(&conf.pg.connect_str).await;
+    
+    let pg = match pg_res {
+        Ok(pool) => pool,
+        Err(err) => panic!("Postgresql Connect Error: {}", err),
+    };
 
     let mut redis_conf = deadpool_redis::Config::from_url(&conf.redis.connect_str);
     redis_conf.pool = Some(deadpool_redis::PoolConfig{
-        max_size: conf.redis.max_size.to_string().parse::<usize>().unwrap(),
+        max_size: conf.redis.max_size.to_string().parse::<usize>().unwrap_or(30),
         timeouts: Timeouts{
             // 等待插槽可用时超时
             wait: Some(Duration::from_secs(conf.redis.idle_timeout)),
@@ -35,7 +40,10 @@ pub async fn new_dbpool(conf: &ServerConfig) -> Result<DBPool, Box<dyn Error>> {
             recycle: Some(Duration::from_secs(conf.redis.idle_timeout)),
         },
     });
-    let redis = redis_conf.create_pool(Some(deadpool_redis::Runtime::Tokio1))?;
+    let redis = match redis_conf.create_pool(Some(deadpool_redis::Runtime::Tokio1)) {
+        Ok(pool) => pool,
+        Err(error) => panic!("Redis Connect Error: {}", error),
+    };
     
     Ok(DBPool{
         pg,
